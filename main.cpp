21a965e7d51406ab7babf4abad0f4e15cf4b249e
avan989 <avan989@yahoo.com>
2019-04-17 14:02:44 -0400

    Update main.cpp
    Combine all files

--- main.cpp
+++ main.cpp
@@ -16,6 +16,9 @@ void makeDir(string);
 void removeDir(string);
 void getStat(string);
 int doesPathExist(string);
+void read( string);
+void write( string );
+void copy(string,string);
 
 int main(int argc, const char * argv[]) {
     
@@ -24,21 +27,20 @@ int main(int argc, const char * argv[]) {
         exit(-1);
     }
     
-    
-    if(strcmp(argv[1], "createDir") == 0){
+    if(strcmp(argv[1], "createDir") == 0){				//createDir
 
         makeDir(argv[2]);
         
     }
-    else if(strcmp(argv[1], "deleteDir") == 0){
+    else if(strcmp(argv[1], "deleteDir") == 0){				//deleteDir
 
         removeDir(argv[2]);
     }
-    else if(strcmp(argv[1], "get") == 0){
+    else if(strcmp(argv[1], "get") == 0){				//get
 
         getStat(argv[2]);
     }
-    else if(strcmp(argv[1], "set") == 0){
+    else if(strcmp(argv[1], "set") == 0){				//set
         
        if (argv[3] == NULL){
        cout << "Need premission input";
@@ -49,46 +51,115 @@ int main(int argc, const char * argv[]) {
 
 
     }
-    else if(strcmp(argv[1], "move") == 0){
+    else if(strcmp(argv[1], "move") == 0){				//move
         
         //set(argv[2],argv[3]);
+	//Rasheed
+	try {
+            std::filesystem::rename("from.txt", "to.txt");
+        } catch (std::filesystem::filesystem_error& e) {
+            std::cout << e.what() << '\n';
+        }
         
     }
-    else if(strcmp(argv[1], "read") == 0){
+    else if(strcmp(argv[1], "read") == 0){				//read
         
-        //read(argv[2]);
-        
-    }
-    else if(strcmp(argv[1], "write") == 0){
-        
-        //write(argv[2]);
+        read(argv[2]);
         
     }
-    else if(strcmp(argv[1], "close") == 0){
-        
-        //close(argv[2]);
+    else if(strcmp(argv[1], "write") == 0){                             //write
+        write(argv[2]);
         
     }
-    else if(strcmp(argv[1], "open") == 0){
+    else if(strcmp(argv[1], "copy") == 0){				//copy
+        
+        copy(argv[2],argv[3]);
+        
+    }
+    else if(strcmp(argv[1], "open") == 0){                             //open
         
         //open(argv[2]);
+	//Rasheed
+	FILE *fptr;
+        char file[15];
+        char c;
+        
+        printf("Type name of file to be opened \n");
+        scanf("%s", file);
+        /*  open the file for reading */
+        fptr = fopen(file, "r");
+        if (fptr == NULL)
+        {
+            printf("Error: Cannot open file \n");
+            exit(0);
+        }
+        c = fgetc(fptr);
+        while (c != EOF)
+        {
+            printf ("%c", c);
+            c = fgetc(fptr);
+        }
+        fclose(fptr);
         
     }
-    else if(strcmp(argv[1], "deleteFile") == 0){
+    else if(strcmp(argv[1], "deleteFile") == 0){                                 //deleteFile
         
         //deleteFile(argv[2]);
+	//Rasheed
+	int status;
+        char file_name[25];
+        
+        cout << "Enter name of a file you wish to delete\n";
+        gets(file_name);
+        
+        status = remove(file_name);
+        
+        if (status == 0)
+            cout << "%s file deleted successfully.\n", file_name;
+        else
+        {
+            cout << "Unable to delete the file\n";
+            perror("Following error occurred");
+        }
         
     }
-    else if(strcmp(argv[1], "createFile") == 0){
+    else if(strcmp(argv[1], "createFile") == 0){                             //createFile
         
         //createFile(argv[2]);
+	//Rasheed
+	int filename;
+        cout << "Enter file name"; cin >> filename;
+        ofstream myfile;
+	
+        myfile.open ("filename.txt");
+        myfile << "Writing this to a file.\n";
+        myfile << "000" << filename;
+        myfile.close();
         
+    } else if(strcmp(argv[1], "help") == 0){                               //help
+        
+        //deleteDir(argv[2]);
+        cout << "createDir\n";
+        cout << "deleteDir\n";
+        cout << "get\n";
+        cout << "set\n";
+        cout << "move\n";
+        cout << "read\n";
+        cout << "write\n";
+        cout << "close\n";
+        cout << "help\n";
+        cout << "open\n";
+        cout << "deleteFile\n";
+        cout << "createFile\n";
+	    
     }else {
 	cout << "incorrect command enter: " << argv[1] << endl;
 	}
     
     return 0;
 }
+
+//Function
 
 int doesPathExist(string path, struct stat &b){
     
@@ -207,6 +278,83 @@ void set(string path, string argv){
     }
 
 }
+
+//Timiriz
+void read( string path){
+
+char buf[1024];
+size_t nbytes;
+ssize_t bytes_read;
+int fd;
+nbytes =sizeof(buf);
+fd=open(path.c_str(),O_RDONLY);
+if(fd==-1){
+cout<<"ERROR IN OPENING FILE ,FILE DOESNOT EXISTS"<<endl;
+exit(-1);
+}
+else{
+bytes_read = read(fd, buf , nbytes);
+cout << buf <<endl;
+close(fd);
+}
+
+}
+ void write(string path){
+int i;
+int fd;
+//char buf[1024];
+string line;
+ssize_t bytes_written;
+size_t nbytes;
+nbytes =sizeof (line);
+fd= open(path.c_str(),O_WRONLY | O_CREAT |O_APPEND |O_TRUNC|S_IRUSR|S_IWUSR);
+if(fd == -1){
+cout<<"permission denied...cannot open a file"<<endl;
+}
+else
+{
+
+cout << "Start Writing\n";
+
+while(true){
+
+
+	cin>>line;
+  	if(strcmp(line.c_str(), "quit!!!") == 0)break;
+	nbytes = sizeof(line.c_str());
+	write(fd,line.c_str(),nbytes);
+        
+}
+
+close(fd);
+}
+
+
+}
+ void copy(string paths,string pathd){
+int fdi,fdo,openflags;
+mode_t fileper;
+char buf[1024];
+size_t nbytes;
+ssize_t readnum;
+nbytes= sizeof(buf);
+ fdi=open(paths.c_str(),O_RDONLY);
+if(fdi==-1){
+cout<<"error in opening file"<<endl;
+}
+openflags = O_CREAT|O_WRONLY|O_TRUNC;
+fileper = S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH;
+fdo =open (pathd.c_str(),openflags,fileper);
+if(fdo==-1){
+cout<<"error in opening file"<<endl;
+}
+while((readnum = read(fdi,buf,nbytes)>0)){
+   write(fdo,buf,nbytes);
+}
+close(fdi);
+close(fdo);
+}
+
 
 
 
